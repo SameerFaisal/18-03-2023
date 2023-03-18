15cc10ad779ec5f772750c0eb7bf724fadec2ca9
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-18 13:05:19 +0500

    18-03-2023

---
+++ 18-03-2023 (2212C2)/app.js
@@ -0,0 +1,40 @@
+//Multidimensional Arrays
+
+// var stdData=[["Zaid",10,"Karachi"],["Shaheer",100,"Faisalabad"],["Rabia",0.75,"Lahore"],["Murtaza",0,"Islamabad"],["Ahtesham",255,"Rawalpindi"]]
+
+// document.write(stdData.length)
+
+
+
+// var stdData=[["Zaid",10,["Karachi","Multan",["Quetta","Peshawar","Delhi"]]],["Shaheer",100,"Faisalabad"],["Rabia",0.75,"Lahore"],["Murtaza",0,"Islamabad"],["Ahtesham",255,"Rawalpindi"]]
+
+// document.write(stdData[0][2][2][1])
+
+
+// document.write(stdData[0][0]+" lives in "+stdData[0][2][2][3]+" and his age is "+stdData[2][1])
+
+
+//Ahtesham = Zaid lives in undefined and his age is 0.75
+//Mubeen = Zaid lives undefined and his age is 0.75
+//Abdullah = undefined
+//Hanif = Zaid lives in undefined and his age is 0.75
+//Shaheer = undefined lives in undefined and age is 0.75
+//Abbas = Zaid lives in peshawar and his is 0.75  
+
+
+// let names=["Zaid","Mubeen","Shaheer"]
+
+// for(let i in names){
+//     document.write(names[i]+"<br>")
+// }
+
+// let nums=[1,2,3,4,54,5,6]
+
+// for(let values in nums){
+//     document.write(nums[values]+"<br>")
+// }
+
+
+
+
+
